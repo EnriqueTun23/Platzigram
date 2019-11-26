1472b0aec735c8cb23a1eb9ccdaa1d62b846f346
Enrique Tun <quiquejesus23@hotmail.com>
2019-11-25 23:42:44 -0600

    Se empezo a trabajar el tema de middlewares

--- users/views.py
+++ users/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Exception
+from django.db.utils import IntegrityError
 
 def login_view(request):
     """ Login view. """
@@ -23,6 +29,29 @@ def login_view(request):
 
 def signup(request):
     """ Sing up view. ejemplo de agregar """
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd1 =  request.POST['passwd']
+        passwd2 = request.POST['passwd_confirmation']
+
+        if passwd1 != passwd2:
+            return render(request, 'users/signup.html', {'error': 'password confirmation no hizo match'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=passwd1)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'El usuario ya esta registrado'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+
     return render(request, 'users/signup.html')
 
 
